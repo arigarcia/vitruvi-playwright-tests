b4590945c9647cf58b6465aaa652f87f21369919
Ariana Garcia <ariana.garcia@d3sistemas.com.ar>
2023-05-16 20:44:11 -0300

    Do not open report automatically

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ dotenv.config({ path: path.resolve(__dirname, '..', `${process.env.TEST_ENVIRONM
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './tests',
+  testDir: './tests/',
   /* Maximum time one test can run for. */
   timeout: 0 * 1000,
   expect: {
@@ -36,7 +36,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
